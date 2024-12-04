a914823063e5146a119b56db9c046d7386849018
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-12-04 20:32:25 +0000

    casi listo faltan relationship

--- src/models.py
+++ src/models.py
@@ -1,29 +1,60 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    subscription_date = Column(Date)
+    favs = relationship('Favorite', backref="User")
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+
+class Character(Base):
+    __tablename__ = 'characters'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String, nullable=False)
+    character_img = Column(String, nullable=False)
+    birth_year = Column(Date)
+    species = Column(String, nullable=False)
+    height = Column(String, nullable=False)
+    mass = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    hair_color = Column(String, nullable=False)
+    skin_color = Column(String, nullable=False)
+    # homeworld_id =
+
+class Planet(Base):
+    __tablename__ = 'planets'
+
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String, nullable=False)
+    planet_img = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    rotation = Column(String, nullable=False)
+    orbital = Column(String, nullable=False)
+    diameter = Column(String, nullable=False)
+    gravity = Column(String, nullable=False)
+    terrain = Column(String, nullable=False)
+    surface = Column(String, nullable=False)
+    climate = Column(String, nullable=False)
+
+
 
     def to_dict(self):
         return {}
