9b7ab1bff8700424a93e461228c268b6c5d8a127
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-12-04 22:39:34 +0000

    Relaciones corregidas proyecto terminado

--- src/models.py
+++ src/models.py
@@ -11,16 +11,18 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     subscription_date = Column(Date)
-    favs = relationship('Favorite', backref="User")
+    favorites = relationship('Favorite', back_populates='user')
 
 class Favorite(Base):
     __tablename__ = 'favorites'
 
-    id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='favorites')
     planet_id = Column(Integer, ForeignKey('planets.id'))
     character_id = Column(Integer, ForeignKey('characters.id'))
 
@@ -29,30 +31,19 @@ class Character(Base):
 
     id = Column(Integer, primary_key=True)
     character_name = Column(String, nullable=False)
-    character_img = Column(String, nullable=False)
-    birth_year = Column(Date)
-    species = Column(String, nullable=False)
-    height = Column(String, nullable=False)
-    mass = Column(String, nullable=False)
-    gender = Column(String, nullable=False)
-    hair_color = Column(String, nullable=False)
-    skin_color = Column(String, nullable=False)
-    # homeworld_id =
+    species = Column(String)
+    gender = Column(String)
+    homeworld_id = Column(String, ForeignKey('planets.id'))
+    planet = relationship('Planet', back_populates='character') 
 
 class Planet(Base):
     __tablename__ = 'planets'
 
     id = Column(Integer, primary_key=True)
-    planet_name = Column(String, nullable=False)
-    planet_img = Column(String, nullable=False)
-    population = Column(Integer, nullable=False)
-    rotation = Column(String, nullable=False)
-    orbital = Column(String, nullable=False)
-    diameter = Column(String, nullable=False)
-    gravity = Column(String, nullable=False)
-    terrain = Column(String, nullable=False)
-    surface = Column(String, nullable=False)
-    climate = Column(String, nullable=False)
+    characters = relationship('Character', back_populates='planet')
+    planet_img = Column(String)
+    population = Column(Integer)
+    climate = Column(String)
 
 
 
